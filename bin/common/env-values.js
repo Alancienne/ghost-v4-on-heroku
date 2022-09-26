5665b4acd9e4b9a1b0b5fbdc530d8fff997d8a46
Jérémy <jeremyschneider21@gmail.com>
2022-09-26 14:53:41 +0200

    update env-values

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,7 +1,9 @@
 module.exports = {
   mysqlDatabaseUrl:
-    process.env.MYSQL_DATABASE_URL ||
-    process.env.JAWSDB_URL ||
-    process.env.CLEARDB_DATABASE_URL ||
-    process.env.JAWSDB_MARIA_URL,
+    // process.env.MYSQL_DATABASE_URL ||
+    // process.env.JAWSDB_URL ||
+    // process.env.CLEARDB_DATABASE_URL ||
+    // process.env.JAWSDB_MARIA_URL,
+    // process.env.JAWSDB_MARIA_NAVY_URL,
+    process.env.RDS_DB_URL,
 };
